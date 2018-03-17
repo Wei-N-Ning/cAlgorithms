316daa94a701634b33297e6f3075edf8340d4919
Wei Ning <nwcgnw@gmail.com>
2018-03-18 09:55:50 +1300

    - fix tiny test cmake
    - init binary search tree

---
+++ src/chapter1/cciBinaryTree.c
@@ -0,0 +1,5 @@
+//
+// Created by wein on 3/11/18.
+//
+
+#include "cciBinaryTree.h"
--- tests/chapterN/test_routeBetweenNodes.c
+++ tests/chapterN/test_routeBetweenNodes.c
@@ -1,6 +1,15 @@
 
 void RunTinyTests();
 
+////// data model //////
+// node is modeled by a single ascii character
+// nodes are held in a C array with fixed size
+// connection is modeled by a char-int hash map
+// so that:
+// given a node (e.g. 'a'),
+// the hash map stores the
+
+
 void test_something() {
     ;
 }
