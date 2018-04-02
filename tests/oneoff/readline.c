16a834f330174495e88539751fae01a3865d8d5d
Wei Ning <wning@wetafx.co.nz>
2018-04-02 12:53:54 +1200

    - add polymorphism to the example

--- tests/oneoff/readline.c
+++ tests/oneoff/readline.c
@@ -5,29 +5,71 @@
 
 #define LINE_LENGTH 64
 
+struct Line;
+
+typedef void (*ResetFunc)(struct Line *l);
+typedef int (*ReadFunc)(const char *str, struct Line *l);
+typedef int (*CheckFunc)(struct Line *l);
+
 struct Line {
     size_t start;
     size_t end;
     
-    char buf[LINE_LENGTH];
+    void *data;
+
+    ResetFunc reset;
+    ReadFunc read;
+    CheckFunc check;
 };
 
-int ReadLine(const char *str, struct Line *l) {
-    int nvariables = 0;
-    int nchars = 0;
-    for (int i=LINE_LENGTH+1; i--; l->buf[i]=0) ;
+///////////////////////////////////////////////////////////
+
+void ResetString(struct Line *l) {
+    for (int i=LINE_LENGTH+1; i--; ((char *)(l->data))[i]=0) ;
+}
+
+int ReadString(const char *str, struct Line *l) {
     // although the format string specifies 64 characters, 
     // it really expects to read in less than 60 chars.
-    nvariables = sscanf(str + l->start, "%64s", l->buf);
-    if (! nvariables) {
+    return sscanf(str + l->start, "%64s", (char *)(l->data)); 
+}
+
+int CheckString(struct Line *l) {
+    int nchars = 0;
+    if (! (nchars = strlen((char *)(l->data)))) {
         return 0;
     }
-    if (! (nchars = strlen(l->buf))) {
+    if (nchars > LINE_LENGTH - 4) {
         return 0;
     }
-    assert(nchars <= LINE_LENGTH - 4);
     // count in the newline character
     l->start += nchars + 1;
+    return 1;
+}
+
+struct Line CreateString() {
+    struct Line l;
+    l.data = malloc(LINE_LENGTH);
+    l.reset = ResetString;
+    l.read = ReadString;
+    l.check = CheckString;
+    l.start = 0;
+    l.end = 0;
+    return l;
+}
+
+void DeleteString(struct Line *l) {
+    free(l->data);
+    l->data = NULL;
+}
+
+///////////////////////////////////////////////////////////
+
+int ReadLine(const char *str, struct Line *l) {
+    l->reset(l);
+    if (! (l->read(str, l) && l->check(l))) {
+        return 0;
+    }
     return 1;
 }
 
@@ -42,11 +84,10 @@ const char *SUT =\
 
 int main(int argc, char **argv) {
     int n = 0;
-    struct Line l;
-    l.start = 0;
-    l.end = 0;
+    struct Line l = CreateString();
     while (ReadLine(SUT, &l)) {
-        printf("<line>%s</line>", l.buf);
+        printf("<line>%s</line>", (char *)(l.data));
     }
+    DeleteString(&l);
     return 0;
 }
