d65b6e736a66f5e16169c7a9251d2bf18d21fc65
Wei Ning <nwcgnw@gmail.com>
2018-04-05 09:00:09 +1200

    - delete unused oneoff code
    - init bfs

---
+++ tests/adm/chapter5/test_bfs.c
@@ -0,0 +1,18 @@
+//
+// Created by wein on 4/5/18.
+//
+
+#include <assert.h>
+
+#include <admSimpleGraph.h>
+
+void RunTinyTests();
+
+void test_nothing() {
+    assert(1);
+}
+
+int main(int argc, char **argv) {
+    RunTinyTests();
+    return 0;
+}
