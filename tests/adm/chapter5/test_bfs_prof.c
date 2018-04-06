4fd908f438340ce5ccfff9b922146943ae46e216
Wei Ning <nwcgnw@gmail.com>
2018-04-06 23:15:00 +1200

    save

--- tests/adm/chapter5/test_bfs_prof.c
+++ tests/adm/chapter5/test_bfs_prof.c
@@ -49,7 +49,13 @@ static void _BFS(admSimpleGraph_t *G, admSimpleNode_t *start) {
 }
 
 int main(int argc, char **argv) {
-    size_t workloads[] = {10, 20, 50, 100, 500, 1000, 2000, 5000, 10000, 20000, 50000};
+    size_t workloads[] = {
+        100, 500, 1000, 5000,
+//        10000,
+//        50000,
+//        100000,
+//        500000
+    };
     size_t workload = 0;
     admSimpleGraph_t *G = NULL;
     admSimpleNode_t *start = NULL;
