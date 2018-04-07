0cc7a9e9ebec5d67bd1f0607ce57d8ecb65a3cf2
Wei Ning <nwcgnw@gmail.com>
2018-04-07 19:14:35 +1200

    - modify bfs prof script to compare the performance of dfs and bfs

--- plotScripts/adm/chapter5/bfs_dfs_comparison.sh
+++ plotScripts/adm/chapter5/bfs_dfs_comparison.sh
@@ -11,7 +11,19 @@ function generateData() {
 50000 225852.000000
 100000 508037.000000
 500000 3494321.000000
-" > /tmp/_
+" > /tmp/bfs_
+
+    echo "
+# NumElements Time
+100 128.000000
+500 798.000000
+1000 1708.000000
+5000 13745.000000
+10000 29082.000000
+50000 244074.000000
+100000 598736.000000
+500000 3798683.000000
+" > /tmp/dfs_
 }
 
 function doPlot() {
@@ -20,8 +32,8 @@ set key outside top center
 set xtics font ',4'
 set ytics font ',4'
 u=0
-plot '/tmp/_' u 1:2 t 'original' w l lw 2,\
-'' u 1:(v=\$2-u, u=\$2, v) t 'differencing' w l lw 3
+plot '/tmp/bfs_' u 1:2 t 'bfs' w l lw 2,\
+'/tmp/dfs_' u 1:2 t 'dfs' w l lw 2
 " > /tmp/_.gnuplot
     gnuplot -p -c /tmp/_.gnuplot
 }
