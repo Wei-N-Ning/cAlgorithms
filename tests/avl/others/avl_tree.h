00542de68154c0fd8e9fdc7ac84bba9293daa4b7
Wei Ning <nwcgnw@gmail.com>
2018-06-18 11:13:07 +1200

    - working on AVL implementation (copy/paste external source)

---
+++ tests/avl/others/avl_tree.h
@@ -0,0 +1,371 @@
+
+#ifndef AVL_TREE_H_
+#define AVL_TREE_H_
+
+#include <memory>
+#include <cstdlib>
+
+template <class KeyType, class ValueType> class AVLTree {
+ private:
+  enum CompareResult {
+    kMinCmp = -1,
+    kEqCmp = 0,
+    kMaxCmp = 1
+  };
+
+  enum Direction {
+    kLeft = 0,
+    kRight = 1
+  };
+
+ public:
+
+  class Comparable {
+   private:
+    KeyType key;
+    ValueType value;
+
+   public:
+    Comparable(KeyType key, ValueType value) : key(key), value(value) {}
+
+    CompareResult Compare(KeyType key) const {
+      return (key == this->key) ? kEqCmp
+          : ((key < this->key) ? kMinCmp : kMaxCmp);
+    }
+
+    KeyType Key() const {
+      return key;
+    }
+
+    ValueType Value() const {
+      return value;
+    }
+
+    void SetValue(ValueType v) {
+      value = v;
+    }
+  };
+
+  enum BalanceFactor {
+    kE = 0,
+    kL = -1,
+    kR = 1
+  };
+
+  struct Node {
+    explicit Node(Comparable* item) :
+        item(item),
+        balance_factor(kE) {
+      children[kLeft] = nullptr;
+      children[kRight] = nullptr;
+    }
+
+    ~Node() {
+        delete item;
+        item = nullptr;
+
+        if (Left()) {
+            delete Left();
+        }
+        if (Right()) {
+            delete Right();
+        }
+    }
+
+    bool IsLeftImbalance() const {
+      return balance_factor < kL;
+    }
+
+    bool IsRightImbalance() const {
+      return balance_factor > kR;
+    }
+
+    Node* Right() const {
+      return children[kRight];
+    }
+
+    Node* Left() const {
+      return children[kLeft];
+    }
+
+    static Direction Opposite(Direction dir) {
+      return static_cast<Direction>(1 - static_cast<int>(dir));
+    }
+
+    static int RotateOnce(Node*& root, Direction dir) {
+      Direction other_dir = Opposite(dir);
+      Node* old_root = root;
+
+      int height_change = (root->children[other_dir]->balance_factor == 0)
+          ? kHeightNoChange : kHeightChange;
+
+      root = old_root->children[other_dir];
+
+      old_root->children[other_dir] = root->children[dir];
+      root->children[dir] = old_root;
+
+      old_root->balance_factor =
+          -((dir == kLeft) ?
+            --(root->balance_factor) : ++(root->balance_factor));
+
+      return  height_change;
+    }
+
+    static int min(int a, int b) {
+      return  (a < b) ? a : b;
+    }
+
+    static int max(int a, int b) {
+      return  (a > b) ? a : b;
+    }
+
+    static int RotateTwice(Node*& root, Direction dir) {
+      Direction other_dir = Opposite(dir);
+      Node* old_root = root;
+      Node* old_other_dir_subtree = root->children[other_dir];
+
+      root = old_root->children[other_dir]->children[dir];
+
+      old_root->children[other_dir] = root->children[dir];
+      root->children[dir] = old_root;
+
+      old_other_dir_subtree->children[dir] = root->children[other_dir];
+      root->children[other_dir] = old_other_dir_subtree;
+
+      root->Left()->balance_factor =
+          -max(static_cast<int>(root->balance_factor), 0);
+      root->Right()->balance_factor =
+          -min(static_cast<int>(root->balance_factor), 0);
+      root->balance_factor = 0;
+
+      return kHeightChange;
+    }
+
+    static Comparable* Get(KeyType key, Node* root, CompareResult cmp) {
+      CompareResult result;
+      while (root &&  (result = root->Compare(key, cmp))) {
+        root = root->children[(result < 0) ? kLeft : kRight];
+      }
+      return root ? root->item : nullptr;
+    }
+
+    static Comparable* Insert(Comparable* item, Node*& root) {
+      int change;
+      return Insert(item, root, change);
+    }
+
+    static Comparable* Insert(Comparable* item, Node*& root, int& change) { // NOLINT
+      if (root == nullptr) {
+        root = new Node(item);
+        change = kHeightChange;
+        return nullptr;
+      }
+
+      Comparable* found = nullptr;
+      CompareResult result = root->Compare(item->Key());
+      Direction dir = (result == kMinCmp) ? kLeft : kRight;
+
+      int increase = 0;
+      if (result != kEqCmp) {
+        found = Insert(item, root->children[dir], change);
+        if (found) {
+          return found;
+        }
+        increase = result * change;
+      } else  {
+        increase = kHeightNoChange;
+        return root->item;
+      }
+
+      root->balance_factor += increase;
+      change = (increase && root->balance_factor)
+          ? (1 - ReBalance(root)) : kHeightNoChange;
+      return nullptr;
+    }
+
+    static int ReBalance(Node*& root) {
+      int height_change = kHeightNoChange;
+
+      if (root->IsLeftImbalance()) {
+        if (root->Left()->balance_factor == kR) {
+          height_change = RotateTwice(root, kRight);
+        } else {
+          height_change = RotateOnce(root, kRight);
+        }
+      } else if (root->IsRightImbalance()) {
+        if (root->Right()->balance_factor == kL) {
+          height_change = RotateTwice(root, kLeft);
+        } else {
+          height_change = RotateOnce(root, kLeft);
+        }
+      }
+
+      return height_change;
+    }
+
+    static Comparable* Remove(KeyType key, Node*& root, CompareResult cmp) {
+      int change;
+      return Remove(key, root, change, cmp);
+    }
+
+    static Comparable* Remove(const KeyType key,
+                              Node*& root,
+                              int& change,
+                              CompareResult cmp) {
+      if (root == nullptr) {
+        change = kHeightNoChange;
+        return nullptr;
+      }
+
+      Comparable* found = nullptr;
+      int decrease = 0;
+
+      CompareResult result = root->Compare(key, cmp);
+      Direction dir = (result == kMinCmp) ? kLeft : kRight;
+
+      if (result != kEqCmp) {
+        found = Remove(key, root->children[dir], change, cmp);
+        if (! found) {
+          return found;
+        }
+        decrease = result * change;
+      } else  {
+        found = root->item;
+        if ((root->Left() == nullptr) &&
+            (root->Right() == nullptr)) {
+          delete root;
+          root = nullptr;
+          change = kHeightChange;
+          return  found;
+        } else if ((root->Left() == nullptr) ||
+                   (root->Right() == nullptr)) {
+          Node*& toDelete = root;
+          root = root->children[(root->Right()) ? kRight : kLeft];
+          change = kHeightChange;
+          toDelete->children[kLeft] = toDelete->children[kRight] = nullptr;
+          delete  toDelete;
+          toDelete = nullptr;
+          return  found;
+        } else {
+          root->item = Remove(key, root->children[kRight],
+                              decrease, kMinCmp);
+        }
+      }
+      root->balance_factor -= decrease;
+
+      if (decrease) {
+        if (root->balance_factor) {
+          change = ReBalance(root);
+        } else {
+          change = kHeightChange;
+        }
+      } else {
+        change = kHeightNoChange;
+      }
+      return  found;
+    }
+
+    CompareResult Compare(KeyType key, CompareResult cmp = kEqCmp) const {
+      switch (cmp) {
+        case kEqCmp:
+          return item->Compare(key);
+        case kMinCmp:
+          return  (children[kLeft] == nullptr) ? kEqCmp : kMinCmp;
+        default:
+          return (children[kRight] == nullptr) ? kEqCmp : kMaxCmp;
+      }
+    }
+
+    Node* children[2];
+    Comparable* item;
+    int balance_factor;
+
+   private:
+    Node() {}
+    Node(const Node& n) {}
+    Node & operator=(const Node&) {}
+  };
+
+  AVLTree() : root_(nullptr) {
+  }
+
+  virtual ~AVLTree() {
+    if (root_) {
+      delete root_;
+      root_ = nullptr;
+    }
+  }
+
+  Node* Root() const {
+    return root_;
+  }
+
+  void Add(const KeyType key, const ValueType value) {
+    Comparable* item = new Comparable(key, value);
+    Comparable* result = Node::Insert(item, root_);
+    if (result) {
+      result->SetValue(value);
+      delete item;
+    }
+  }
+
+  Comparable* Remove(const KeyType key, CompareResult cmp = kEqCmp) {
+    return Node::Remove(key, root_, cmp);
+  }
+
+  Comparable* Get(const KeyType key, CompareResult cmp = kEqCmp) const {
+    return Node::Get(key, root_, cmp);
+  }
+
+  Comparable* GetLowerNearest(const KeyType key) const {
+    Node* last_node_lt_key = nullptr;
+    Node* n = root_;
+
+    while (n != nullptr) {
+      if (n->item->Key() == key) {
+        return n->item;
+      } else if (n->item->Key() < key) {
+        last_node_lt_key = n;
+        n = n->Right();
+      } else {
+        n = n->Left();
+      }
+    }
+    if (last_node_lt_key == nullptr) {
+      return nullptr;
+    } else {
+      return last_node_lt_key->item;
+    }
+  }
+
+  bool IsBalanced() const {
+    if (root_ == nullptr) {
+      return true;
+    }
+    int diff = abs(Height(root_->Left()) - Height(root_->Right()));
+    return diff <= 1;
+  }
+
+  bool IsEmpty() const {
+    return root_ == nullptr;
+  }
+
+ private:
+  enum HeightEffect {
+    kHeightNoChange = 0,
+    kHeightChange = 1
+  };
+
+  int Height(Node* n) const {
+    if (n == nullptr) {
+      return 0;
+    }
+    int l = Height(n->Left());
+    int r = Height(n->Right());
+    return Node::max(l, r) + 1;
+  }
+
+  Node* root_;
+};
+
+#endif  // AVL_TREE_H_
---
+++ tests/avl/others/test.cpp
@@ -0,0 +1,314 @@
+
+#include "./avl_tree.h"
+
+#include <algorithm>
+#include <string>
+#include <cstdint>
+#include <cassert>
+
+void RunTinyTests();
+
+typedef AVLTree<int, int> IntAVLTree;
+typedef IntAVLTree::Node Node;
+
+void ExpectNodeEq(int expected_key, int8_t expected_factor, const Node* node) {
+    assert(node != nullptr);
+    assert(expected_key == node->item->Key());
+    assert(expected_factor == node->balance_factor);
+}
+
+class AVLTreeTest {
+public:
+    IntAVLTree tree_;
+
+    AVLTreeTest() = default;
+    ~AVLTreeTest() = default;
+
+    IntAVLTree& tree() {
+        return tree_;
+    }
+
+    void MakePreDoubleRightRotationTree() {
+        tree_.Add(5, 5);
+        ExpectNodeEq(5, IntAVLTree::kE, tree_.Root());
+
+        tree_.Add(3, 3);
+        ExpectNodeEq(5, IntAVLTree::kL, tree_.Root());
+        ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left());
+
+        tree_.Add(9, 9);
+        ExpectNodeEq(5, IntAVLTree::kE, tree_.Root());
+        ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left());
+        ExpectNodeEq(9, IntAVLTree::kE, tree_.Root()->Right());
+
+        tree_.Add(7, 7);
+        ExpectNodeEq(5, IntAVLTree::kR, tree_.Root());
+        ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left());
+        ExpectNodeEq(9, IntAVLTree::kL, tree_.Root()->Right());
+        ExpectNodeEq(7, IntAVLTree::kE, tree_.Root()->Right()->Left());
+
+        tree_.Add(10, 10);
+        ExpectNodeEq(5, IntAVLTree::kR, tree_.Root());
+        ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left());
+        ExpectNodeEq(9, IntAVLTree::kE, tree_.Root()->Right());
+        ExpectNodeEq(7, IntAVLTree::kE, tree_.Root()->Right()->Left());
+        ExpectNodeEq(10, IntAVLTree::kE, tree_.Root()->Right()->Right());
+    }
+
+    void MakePreDoubleLeftRotationTree() {
+        tree_.Add(9, 9);
+        tree_.Add(5, 5);
+        tree_.Add(11, 11);
+        tree_.Add(3, 3);
+        tree_.Add(7, 7);
+
+        ExpectNodeEq(9, IntAVLTree::kL, tree_.Root());
+        ExpectNodeEq(5, IntAVLTree::kE, tree_.Root()->Left());
+        ExpectNodeEq(11, IntAVLTree::kE, tree_.Root()->Right());
+        ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left()->Left());
+        ExpectNodeEq(7, IntAVLTree::kE, tree_.Root()->Left()->Right());
+    }
+};
+
+static AVLTreeTest* TEST = nullptr;
+
+void setUp() {
+    TEST = new AVLTreeTest;
+}
+
+void tearDown() {
+    delete TEST;
+    TEST = nullptr;
+}
+
+void test_AddOneNode() {
+    auto& tree_ = TEST->tree();
+    assert(! tree_.Get(1));
+    assert(tree_.IsBalanced());
+    tree_.Add(1, 1);
+    assert(1 == tree_.Get(1)->Value());
+    ExpectNodeEq(1, IntAVLTree::kE, tree_.Root());
+}
+
+void test_AddTwoNodesRightSide() {
+    auto& tree_ = TEST->tree();
+    tree_.Add(3, 3);
+    tree_.Add(5, 5);
+    ExpectNodeEq(3, IntAVLTree::kR, tree_.Root());
+    ExpectNodeEq(5, IntAVLTree::kE, tree_.Root()->Right());
+}
+
+void test_AddThreeNodesRightSide() {
+    auto& tree_ = TEST->tree();
+    tree_.Add(3, 3);
+    tree_.Add(5, 5);
+    tree_.Add(7, 7);
+    ExpectNodeEq(5, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left());
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root()->Right());
+}
+
+void test_DoubleRightRotation1() {
+    auto& tree_ = TEST->tree();
+    TEST->MakePreDoubleRightRotationTree();
+
+    tree_.Add(6, 6);
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(5, IntAVLTree::kE, tree_.Root()->Left());
+    ExpectNodeEq(9, IntAVLTree::kR, tree_.Root()->Right());
+    assert(! tree_.Root()->Right()->Left());
+    ExpectNodeEq(10, IntAVLTree::kE, tree_.Root()->Right()->Right());
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left()->Left());
+    ExpectNodeEq(6,  IntAVLTree::kE, tree_.Root()->Left()->Right());
+}
+
+void test_DoubleRightRotation2() {
+    auto& tree_ = TEST->tree();
+    TEST->MakePreDoubleRightRotationTree();
+
+    tree_.Add(8, 8);
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(5, IntAVLTree::kL, tree_.Root()->Left());
+    ExpectNodeEq(9, IntAVLTree::kE, tree_.Root()->Right());
+    ExpectNodeEq(8, IntAVLTree::kE, tree_.Root()->Right()->Left());
+    ExpectNodeEq(10, IntAVLTree::kE, tree_.Root()->Right()->Right());
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left()->Left());
+    assert(! tree_.Root()->Left()->Right());
+}
+
+void test_DoubleLeftRotation1() {
+    auto& tree_ = TEST->tree();
+    TEST->MakePreDoubleLeftRotationTree();
+
+    tree_.Add(6, 6);
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(5, IntAVLTree::kE, tree_.Root()->Left());
+    ExpectNodeEq(9, IntAVLTree::kR, tree_.Root()->Right());
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left()->Left());
+    ExpectNodeEq(6, IntAVLTree::kE, tree_.Root()->Left()->Right());
+    assert(! tree_.Root()->Right()->Left());
+    ExpectNodeEq(11, IntAVLTree::kE, tree_.Root()->Right()->Right());
+}
+
+void test_DoubleLeftRotation2() {
+    auto& tree_ = TEST->tree();
+    TEST->MakePreDoubleLeftRotationTree();
+
+    tree_.Add(8, 8);
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(5, IntAVLTree::kL, tree_.Root()->Left());
+    ExpectNodeEq(9, IntAVLTree::kE, tree_.Root()->Right());
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root()->Left()->Left());
+    assert(! tree_.Root()->Left()->Right());
+    ExpectNodeEq(11, IntAVLTree::kE, tree_.Root()->Right()->Right());
+    ExpectNodeEq(8, IntAVLTree::kE, tree_.Root()->Right()->Left());
+}
+
+void test_RightRotation() {
+    auto& tree_ = TEST->tree();
+    TEST->MakePreDoubleRightRotationTree();
+
+    tree_.Add(6, 6);
+    ExpectNodeEq(7, IntAVLTree::kE, tree_.Root());
+
+    tree_.Add(2, 2);
+    assert(IntAVLTree::kL == tree_.Root()->balance_factor);
+    assert(5 == tree_.Root()->Left()->item->Key());
+    assert(IntAVLTree::kL == tree_.Root()->Left()->balance_factor);
+    assert(3 == tree_.Root()->Left()->Left()->item->Key());
+    assert(IntAVLTree::kL == tree_.Root()->Left()->Left()->balance_factor);
+    assert(2 == tree_.Root()->Left()->Left()->Left()->item->Key());
+    assert(IntAVLTree::kE ==
+           tree_.Root()->Left()->Left()->Left()->balance_factor);
+
+    tree_.Add(1, 1);
+    assert(7 == tree_.Root()->item->Key());
+    assert(IntAVLTree::kL == tree_.Root()->balance_factor);
+
+    assert(IntAVLTree::kL == tree_.Root()->Left()->balance_factor);
+    assert(5 == tree_.Root()->Left()->item->Key());
+
+    assert(IntAVLTree::kE == tree_.Root()->Left()->Left()->balance_factor);
+    assert(2 == tree_.Root()->Left()->Left()->item->Key());
+
+    assert(IntAVLTree::kE ==
+           tree_.Root()->Left()->Left()->Left()->balance_factor);
+    assert(1 == tree_.Root()->Left()->Left()->Left()->item->Key());
+
+    assert(IntAVLTree::kE ==
+           tree_.Root()->Left()->Left()->Right()->balance_factor);
+    assert(3 == tree_.Root()->Left()->Left()->Right()->item->Key());
+}
+
+void test_InsertMany() {
+    auto& tree_ = TEST->tree();
+    const int kN = 1000;
+    for (int i = 1; i <= kN; i++) {
+        tree_.Add(i, i);
+        assert(tree_.IsBalanced());
+    }
+}
+
+void test_Regression() {
+    auto& tree_ = TEST->tree();
+    tree_.Add(2, 2);
+    ExpectNodeEq(2, IntAVLTree::kE, tree_.Root());
+
+    tree_.Add(4, 4);
+    ExpectNodeEq(2, IntAVLTree::kR, tree_.Root());
+    ExpectNodeEq(4, IntAVLTree::kE, tree_.Root()->Right());
+
+    tree_.Add(3, 3);
+    ExpectNodeEq(3, IntAVLTree::kE, tree_.Root());
+    ExpectNodeEq(2, IntAVLTree::kE, tree_.Root()->Left());
+    ExpectNodeEq(4, IntAVLTree::kE, tree_.Root()->Right());
+    tree_.Add(1, 1);
+}
+
+void test_Regression2() {
+    auto& tree_ = TEST->tree();
+    tree_.Add(364, 2);
+    tree_.Add(919, 2);
+    tree_.Add(915, 2);
+    tree_.Add(825, 2);
+    tree_.Add(560, 2);
+    tree_.Add(449, 2);
+    tree_.Add(425, 2);
+    tree_.Add(160, 2);
+    tree_.Add(409, 2);
+    tree_.Add(423, 2);
+    tree_.Add(727, 2);
+    assert(tree_.IsBalanced());
+}
+
+void test_InsertRandom() {
+    auto& tree_ = TEST->tree();
+    auto seed = (unsigned int)clock();
+    srand(seed);
+    const int kN = 1000;
+    for (int i = 1; i <= kN; i++) {
+        auto v = rand() % 1000;
+        tree_.Add(v, v);
+        assert(tree_.IsBalanced());
+    }
+}
+
+void test_DeleteOnlyOne() {
+    auto& tree_ = TEST->tree();
+    tree_.Add(1, 1);
+    tree_.Remove(1);
+    assert(tree_.IsEmpty());
+}
+
+void xtest_RemoveMany() {
+    auto& tree_ = TEST->tree();
+//    tree_.Add(364, 2);
+    tree_.Add(919, 2);
+    tree_.Add(915, 2);
+//    tree_.Add(825, 2);
+//    tree_.Add(560, 2);
+//    tree_.Add(449, 2);
+//    tree_.Add(425, 425);
+//    tree_.Add(160, 160);
+    tree_.Add(409, 409);
+//    tree_.Add(423, 423);
+//    tree_.Add(727, 727);
+//    assert(423 == tree_.GetLowerNearest(424)->Value());
+//    assert(425 == tree_.GetLowerNearest(425)->Value());
+//    assert(727 == tree_.GetLowerNearest(728)->Value());
+//    assert(! tree_.GetLowerNearest(2));
+
+//    tree_.Remove(9999);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(364);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(825);
+//    assert(tree_.IsBalanced());
+    tree_.Remove(915);
+
+
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(919);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(560);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(449);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(425);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(160);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(423);
+//    assert(tree_.IsBalanced());
+//    assert(409 == tree_.Get(409)->Value());
+//    tree_.Remove(409);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(727);
+//    assert(tree_.IsBalanced());
+//    tree_.Remove(727);
+}
+
+int main(int argc, char **argv) {
+    RunTinyTests();
+    return 0;
+}
